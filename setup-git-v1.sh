1210a8f8d23519fdd6c06f7d0d00e25b98ab8bd9
Adarsh Bhat <kanakbhat6@gmail.com>
2020-04-10 21:00:45 +0530

    Comment : checking on Friday 10 April 2020 09:00:45 PM IST

--- setup-git-v1.sh
+++ setup-git-v1.sh
@@ -61,8 +61,8 @@ fi
 #https://www.atlassian.com/git/tutorials/setting-up-a-repository/git-config
 
 git init
-git config --global user.name "Erik Dubois"
-git config --global user.email "erik.dubois@gmail.com"
+git config --global user.name "Adarsh Bhat"
+git config --global user.email "kanakbhat6@gmail.com"
 sudo git config --system core.editor nano
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=18000'
