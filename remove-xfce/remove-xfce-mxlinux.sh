3e920bae627c78eee590528284544d1f95dbc279
Adarsh Bhat <kanakbhat6@gmail.com>
2020-04-10 23:00:06 +0530

    Comment : v3,3 on Friday 10 April 2020 11:00:06 PM IST

---
+++ remove-xfce/remove-xfce-mxlinux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#It’s not totally obvious how to get rid of Xfce, so user kmathern came up with this single command:
+
+dpkg -l | grep .xfce. | awk '{print $2}' | xargs sudo apt-get purge -V --auto-remove -yy 
+
+#When that has finished, then reinstall one package:
+
+sudo apt install desktop-defaults-mx-common
+
