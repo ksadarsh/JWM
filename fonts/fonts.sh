41b0719b9f2fd1b2d688b02937f0d1ef5247b82a
Adarsh Bhat <kanakbhat6@gmail.com>
2020-05-01 23:07:37 +0530

    Comment : 3.5.24 on Fri May  1 23:07:37 IST 2020

--- fonts/fonts.sh
+++ fonts/fonts.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sudo cp -rf *.ttf /usr/share/fonts
-sudo cp -rf *.otf /usr/share/fonts
+sudo mv *.ttf /usr/share/fonts
+sudo mv *.TTF /usr/share/fonts
+sudo mv *.otf /usr/share/fonts
 fc-cache -fv
 sudo fc-cache -fv
